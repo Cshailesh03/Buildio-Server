e40c99a6bd41f0e9ebe18aedb5785d3a4e4d0fdc
Cshailesh03 <chhalotreshailesh@gmail.com>
2025-05-16 12:21:41 +0530

    move server.js from src to root directory for standard entry point structure

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import dotenv from "dotenv";
 dotenv.config({path: '../env'});
-import connectDB from './config/db.js';
+import connectDB from './src/config/db.js';
 import express from "express";
-import {app} from './app.js'
+import {app} from './src/app.js'
 
 
 connectDB()
